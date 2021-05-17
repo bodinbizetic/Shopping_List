35e465013e4da789f47f620b7f4992d8bc9295b4
maslarevicolga <olga.maslarevic1@gmail.com>
2021-05-17 21:30:47 +0200

    Add ItemPriceModel

---
+++ Implemetacija/app/Models/ItemPriceModel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+
+
+class ItemPriceModel extends Model
+{
+    protected $table      = 'item';
+    protected $primaryKey = array('idItem', 'idShopChain');
+    protected $useAutoIncrement = true;
+    protected $returnType     = 'array';
+    protected $allowedFields = ['price'];
+
+
+    protected $validationRules    = [
+        'idItem'     => 'required|in_db[`item`]',
+        'idShopChain'     => 'required|in_db[`shopchain`]'
+    ];
+
+
+    protected $skipValidation     = false;
+
+
+
+
+}
