1189c9fb9b4bbca857df83d1c8edd850d0319168
maslarevicolga <olga.maslarevic1@gmail.com>
2021-05-17 21:45:19 +0200

    Add InGroupModel

---
+++ Implemetacija/app/Models/InGroupModel.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+
+
+class InGroupModel extends Model
+{
+    protected $table      = 'shopchain';
+    protected $primaryKey = array('idGroup', 'idUser');
+    protected $useAutoIncrement = true;
+    protected $returnType     = 'array';
+    protected $allowedFields = ['type'];
+
+
+    protected $validationRules    = [
+        'idGroup'     => 'required|in_db[`group`]',
+        'idUser'     => 'required|in_db[`user`]',
+        'type'       => 'required'
+    ];
+
+
+    protected $skipValidation     = false;
+
+
+
+
+}
