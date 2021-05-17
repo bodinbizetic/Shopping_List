e237b847d55d9c40579ebd062d73d58e93e84cd7
maslarevicolga <olga.maslarevic1@gmail.com>
2021-05-17 21:37:37 +0200

    Add ShoppingListModel

--- Implemetacija/app/Models/ShoppingList.php
+++ Implemetacija/app/Models/ShoppingList.php
@@ -1,1 +1,31 @@
 <?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+
+
+class ShopChainModel extends Model
+{
+    protected $table      = 'shoppinglist';
+    protected $primaryKey = 'idShoppingList';
+    protected $useAutoIncrement = true;
+    protected $returnType     = 'array';
+    protected $allowedFields = ['name', 'active', 'idShop'];
+
+
+    protected $validationRules    = [
+        'idShoppingList'     => 'required',
+        'idGroup'     => 'required|in_db[`group`]',
+        'name'     => 'required|max_length[45]',
+        'active'     => 'required'
+    ];
+
+
+    protected $skipValidation     = false;
+
+
+
+
+}
