2955cc013c5f0f2cb817664854eebc14a83d3c00
maslarevicolga <olga.maslarevic1@gmail.com>
2021-05-23 18:54:36 +0200

    Finish notifications

--- Implemetacija/app/Models/NotificationModel.php
+++ Implemetacija/app/Models/NotificationModel.php
@@ -5,18 +5,24 @@ namespace App\Models;
 use CodeIgniter\Model;
 
 
+define("JOIN_GROUP_REQ", ["msg" => "Join group", "type" => 0]);
+define("NEW_GROUP_MEMBER", ["msg" => "New member group", "type" => 1]);
+define("LIST_STATUS", ["msg" => "List status", "type" => 2]);
+
+
 class NotificationModel extends Model
 {
+
     protected $table      = 'notification';
     protected $primaryKey = 'idNotification';
     protected $useAutoIncrement = true;
     protected $returnType     = 'array';
-    protected $allowedFields = ['isRead'];
+    protected $allowedFields = ['isRead', 'type', 'idUser', 'idGroup', 'text'];
 
     protected $validationRules    = [
-        'text'     => 'required|min_length[1]|max_length[256]',
-        'idUser'        => 'required|in_db[`user`]',
-        'idGroup'     => 'required|in_db[`group`]',
+        'text'     => 'max_length[256]',
+        'idUser'        => 'required',
+        'idGroup'     => 'required',
         'type'     => 'required'
     ];
 
