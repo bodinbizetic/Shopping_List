cfce5e1bd73feef7d236ebf1bf7aaa1d6ec40d76
AndrejGobeX <gobex7@gmail.com>
2021-06-03 10:15:24 +0200

    error doc

--- Implemetacija/app/Controllers/Error.php
+++ Implemetacija/app/Controllers/Error.php
@@ -1,11 +1,24 @@
 <?php
-
+/**
+ * Authors - Andrej Gobeljic 0019/2018
+ */
 
 namespace App\Controllers;
 
-
+/**
+ * Class Error - klasa zaduzena za prikaz gresaka
+ *
+ * @package App\Controllers
+ * @version 1.0
+ */
 class Error extends BaseController
 {
+    /**
+     * Funkcija za prikaz greske
+     *
+     * @param $error - Tekst greske
+     * @return void
+     */
     static function show($error){
         echo view("common/header");
         echo view("error", ['error' => $error
